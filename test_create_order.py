13151e665a9750ddb0a273b135d817003b6fa63e
AMarat70 <Mabdulguzhin@gmail.com>
2024-10-06 22:34:43 +0300

    Update test_create_order.py

--- test_create_order.py
+++ test_create_order.py
@@ -1,8 +1,6 @@
-import requests
-#import configuration
-import data
 import sender_stand_request
 
-def test_create_ordrer():
-    get_order = sender_stand_request.get_order()
+def test_create_order():
+    track_order = sender_stand_request.create_order()
+    get_order = sender_stand_request.get_order(track_order)
     assert get_order.status_code == 200
