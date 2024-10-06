1a12232182c994b8393e8bbca4d166f498931522
AMarat70 <Mabdulguzhin@gmail.com>
2024-10-06 19:26:00 +0300

    Delete test_create_order.py

--- test_create_order.py
+++
@@ -1,9 +0,0 @@
-import requests
-import configuration
-import data
-import sender_stand_request
-
-def test_create_ordrer():
-    order_track = sender_stand_request.create_order(data.order_body)
-    get_orders = requests.get(configuration.URL_SERVICE + configuration.ORDERS_PATH + str(order_track))
-    assert get_orders.status_code == 200
