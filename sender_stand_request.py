610a62c0362713f427c6de05bafe3d84e080e112
AMarat70 <Mabdulguzhin@gmail.com>
2024-10-06 22:23:32 +0300

    Update sender_stand_request.py

--- sender_stand_request.py
+++ sender_stand_request.py
@@ -2,13 +2,12 @@ import configuration
 import requests
 import data
 
-def create_order(body):
+def create_order():
     response_create_order = requests.post(configuration.URL_SERVICE + configuration.CREATE_ORDERS_PATH,
                          json=data.order_body,
                          headers=data.headers)
     return response_create_order.json()["track"]
 
-def get_order():
-    order_track = create_order(data.order_body)
+def get_order(order_track):
     response_get_order = requests.get(configuration.URL_SERVICE + configuration.ORDERS_PATH + str(order_track))
     return response_get_order
